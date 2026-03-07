38a90fa62bd697e66e943877c364648c57e9a541
Gustavo Goes <gustavorochamacie12007@gmail.com>
2026-03-07 00:25:38 -0300

    alterando index.js

--- minha-api/index.js
+++ minha-api/index.js
@@ -1,32 +1,17 @@
 const express = require('express');
 const app = express();
 
+const viagensRoutes = require('./routes/viagens');
+
 app.use(express.json());
 
-// "Banco de dados" temporário
-let viagens = [];
-
-// Rota para testar a API
+// rota de teste
 app.get('/', (req, res) => {
   res.send('API da Agencia de Viagens funcionando!');
 });
 
-// Listar viagens
-app.get('/viagens', (req, res) => {
-  res.json(viagens);
-});
-
-// Cadastrar viagem
-app.post('/viagens', (req, res) => {
-  const viagem = req.body;
-
-  viagens.push(viagem);
-
-  res.json({
-    mensagem: 'Viagem cadastrada com sucesso',
-    viagem: viagem,
-  });
-});
+// usar as rotas de viagens
+app.use('/viagens', viagensRoutes);
 
 const PORT = 3000;
 
