166e82554f2ecf3611dc60a0f7b5a766790a0d38
Gustavo Goes <gustavorochamacie12007@gmail.com>
2026-03-11 00:15:48 -0300

    removendo bugs.

--- minha-api/routes/viagens.js
+++ minha-api/routes/viagens.js
@@ -1,9 +1,12 @@
-import express from "express";
+import express from 'express';
 
 const app = express();
 
-app.get('/usuarios', ((req, res) => { 
+app.get('/usuarios', (req, res) => {
+  res.send('Deu certo');
+});
 
+app;
 /* 1) tipo de rota / metodo http
 2) endereco da rota
 
